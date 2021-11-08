0764f2dd3d5113d5abd91c79c5cdec9e37fe9606
JoaoVictor55 <jaovictor456@outlook.com.br>
2021-11-08 10:53:58 -0300

    a

---
+++ pacoteHTTP.java
@@ -0,0 +1,13 @@
+import org.abstract_fatory.AbstractPacote;
+
+public class pacoteHTTP extends AbstractPacote
+{
+    public  void prepararDados()
+    {
+        System.out.print("Usando HTTP");
+    }
+    public  void criptografar()
+    {
+        System.out.print("Criptografando com HTTP");
+    } 
+}
