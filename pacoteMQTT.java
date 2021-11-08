c83fcc41a3136217227436b78d02ed689ec84dbf
JoaoVictor55 <jaovictor456@outlook.com.br>
2021-11-08 11:02:46 -0300

    vai

---
+++ pacoteMQTT.java
@@ -0,0 +1,11 @@
+public class pacoteMQTT extends AbstractPacote
+{
+    public  void prepararDados()
+    {
+        System.out.print("Usando MQTT");
+    }
+    public  void criptografar()
+    {
+        System.out.print("Criptografando com MQTT");
+    } 
+}
